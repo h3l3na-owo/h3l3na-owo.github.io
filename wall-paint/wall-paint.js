79cb2b39424f53ebd4ca2cc38812ae061d0eda7b
xicobarbo <redgreeneco@gmail.com>
2020-08-31 17:08:36 -0400

    wall-paint created

---
+++ wall-paint/wall-paint.js
@@ -0,0 +1,68 @@
+const canvas = document.querySelector('canvas');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
+const position = document.querySelector('#position');
+const colorPicker = document.querySelector('select#color');
+let color = 'rgb(20,250,20)'; 
+let e = window.event;
+let isDrawing = false;
+
+//let posX = e.clientX;
+//let posY = e.clientY;
+
+
+function degToRad(degrees) {
+  return degrees * Math.PI / 180;
+};
+
+function circle(x, y){
+	console.log("circle");
+	ctx.fillStyle = color;
+	//ctx.strokeStyle = 'rgb(red, green, blue)';
+	ctx.beginPath();
+	ctx.arc(x, y, 25, degToRad(0), degToRad(360), false);
+	ctx.fill();
+}
+
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
+
+
+function trackMouse(evt){
+  var mousePos = getMousePos(canvas, evt);
+  var message = mousePos.x + ',' + mousePos.y;
+  var mousePos = getMousePos(canvas, evt);
+  position.textContent = 'X;Y:'+ message;
+  if(isDrawing === true){
+    circle(mousePos.x, mousePos.y);
+    console.log("draw");
+  }
+}
+canvas.addEventListener('mousedown', function(){
+   console.log('event');
+   isDrawing = true;
+});
+
+canvas.addEventListener('mouseup', function(){
+   console.log('event2');
+   isDrawing = false;      
+});
+
+canvas.addEventListener('mousemove', trackMouse, false);
+
+
+colorPicker.addEventListener("change", function(){
+  color = colorPicker.value;
+});
+
+document.querySelector('#resetButton').addEventListener("click", function() {
+    ctx.clearRect(0,0,width,height);
+  });
+   
+      
