c095c2bb56dda11818695da617d25600275f3acc
xicobarbo <redgreeneco@gmail.com>
2021-02-16 17:59:46 -0500

    formatting, api call

--- record-store/record-store.js
+++ record-store/record-store.js
@@ -4,6 +4,8 @@ const parsedHash = new URLSearchParams(
   window.location.hash.substr(1) // skip the first char (#)
 );
 
+const parsedHost = window.location.hostname;
+
 console.log("yo:"+parsedHash.get("access_token"));
 var authToken = parsedHash.get("access_token");
 
@@ -14,8 +16,15 @@ var spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken(authToken);
 
 var searchNew = document.querySelector("#search-new");
-
 var getAnother = document.querySelector("#get-another");
+var searchedSong = document.querySelector("#searchedSong");
+var authButton = document.querySelector('#reauth');
+var mainText = document.querySelector('#main');
+var yesButton = document.querySelector('#yes');
+var songLink = document.querySelector("#song-link");
+var searchBar= document.querySelector('#search');
+
+var isAuth=false;
 
 searchNew.addEventListener("click", function(){
 	searchNew.textContent="it worked!";
@@ -27,11 +36,103 @@ getAnother.addEventListener("click", function(){
 	console.log("get another test:"+getAnother.textContent);
 });
 
+function test(){
+	var searchedGenre = 'genre:"metal"';
+	var yearRange = 'year:0000-9999 ';
+	var artistName;
+
+	spotifyApi.searchArtists(yearRange+searchedGenre).then(
+	  function (data) {
+	    console.log('all artists:', data);
+	    artistNum = data.artists.total;
+	    if (artistNum > 500){
+	    	artistNum = 501;
+	    }
+
+	    // get random artist
+	    limit = 1;
+		offset = Math.floor(Math.random()*artistNum);
+		spotifyApi.searchArtists(yearRange+searchedGenre,{limit:limit , offset:offset}).then(
+		  function (data) {
+		  	//artist = data;
+		  	//artistName = artist.artists.items[0].name;
+		  	//artistTitle.textContent = artistName;
+		  	displayArtist(data.artists.items[0]);
+		    //console.log(artistNum);
+		    
+		  },
+		  function (err) {
+		    console.error(err);
+		    reAuth();
+		  }
+		); 
+
+	  },
+	  function (err) {
+	    console.error(err);
+	    reAuth();
+	  }
+	);
+}
+
+
+yesButton.addEventListener('click', search1);
+
+window.addEventListener('load', function(){
+	songLink.style.display = 'none';
+	searchNew.style.display = 'none';
+	getAnother.style.display ='none';
+	searchedSong.style.visibility = 'hidden';
+	searchBar.style.visibility = 'hidden';
+})
+
+function displayArtist(artist){
+	isAuth=true;
+	searchedSong.textContent = artist.name;
+	console.log(searchedSong.textContent);
+	console.log("auth: "+isAuth);
+	if (isAuth === true){
+		introScreen();
+	}
+}
+
+function introScreen(){
+	authButton.style.display = 'none';
+	mainText.innerHTML="Sup! Welcome to my record store. Give me a song and I'll give you another. You ready?";
+	yesButton.value= "yes";
+	console.log(mainText);
+}
+
+function search1(){
+	console.log("screen twoooooo");
+	mainText.innerHTML = "Alright, type in your song!";
+	searchBar.style.visibility = 'visible';
+	yesButton.display = "none";
+}
+
+function reAuth(){
+	console.log('reAuth!');
+	authButton.style.display = 'block';
+	if (parsedHost === '0.0.0.0'){
+		authButton.href = "https://accounts.spotify.com/en/authorize?client_id=2e53a5b23a78477e99eae192a230276c&response_type=token&redirect_uri=http://0.0.0.0:8000";
+	}
+	else if (parsedHost === "h3l3na-owo.github.io"){
+		authButton.href = "https://accounts.spotify.com/en/authorize?client_id=163f41ea630d40358a5b6c7a98d5134b&response_type=token&redirect_uri=https:%2F%2Fh3l3na-owo.github.io/artist-gen/"
+	}
+}
+
+
+test();
+
 /* TO DO: 
 make page recognize reauthentication
+figure out how to make reauth button not take up space
 
 set up drop-down search
+
 set up audio player	
+
+change non-local (production) reauth url
 
 */
 
